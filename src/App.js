aff903d288e78f7ab48ee4d684d9315f9be03307
oscarva@stud.ntnu.no <oscarva@stud.ntnu.no>
2023-02-18 13:55:52 +0100

    data is sorted by danger type, now it displays only incidents with orange danger level

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 import React, { useState, useEffect } from "react";
 
+function sortByDangerLevel(array)
+{
+  let sortedIncidents = []
+  let dangerLevelIncidents = []
+  let keyValues = Object.keys(array)
+  let incident;
+  let dangerLevel = ""
+
+  for (let i = 0; i < keyValues.length; i++)
+  {
+    
+    incident = array[keyValues[i]].getElementsByTagName("title")[0]
+    dangerLevel = incident.textContent.split(",")[1]
+    if (dangerLevel.includes("nivå"))
+    {
+
+        dangerLevelIncidents.push(array[keyValues[i]])
+    }
+  }
+
+  dangerLevelIncidents.forEach((value) => 
+  {
+      incident = value.getElementsByTagName("title")[0]
+      dangerLevel = incident.textContent.split(",")[1]
+      if (dangerLevel.includes("oransje"))
+      {
+        sortedIncidents.push(value)
+      }
+  })
+
+  return sortedIncidents
+
+}
+
 function App() {
   const [nodeContent, setNodeContent] = useState([]);
 
@@ -8,16 +42,29 @@ function App() {
       .then((resp) => resp.text())
       .then((result) => {
         let xmlData = new window.DOMParser().parseFromString(result, "text/xml");
-
         let titleArray = xmlData.getElementsByTagName("item");
+        console.log(titleArray)
+        let orangeIncidents = sortByDangerLevel(titleArray)
 
         let nodes = [];
+        /*
         let elem = titleArray[0];
         elem.childNodes.forEach((node) => {
           nodes.push(node.textContent);
         });
         setNodeContent(nodes);
+*/
+        orangeIncidents.forEach(value => 
+          {
+              value.childNodes.forEach(node => 
+                {
+                    nodes.push(node.textContent)
+                })
+          })
+        setNodeContent(nodes)
+
       });
+
   }, []);
 
   return (
