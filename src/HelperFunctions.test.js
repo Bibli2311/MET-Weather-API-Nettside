9412ee4aef99d8b9792e51aa7229179a8e0006e3
Bibli2311 <oscar.vagle@yahoo.no>
2023-02-20 09:44:30 +0100

    Commit made from VS Code Azure Static Web Apps

--- src/HelperFunctions.test.js
+++ src/HelperFunctions.test.js
@@ -1,27 +1,11 @@
 import { sortByDangerLevel } from "./HelperFunctions";
+import { fetchData } from "./HelperFunctions";
 
-describe("sortByDangerLevel function", () => {
-    const testArray = {
-      "1": {
-        getElementsByTagName: (tag) => ({
-          textContent: "Kansellert Sterk ising på skip, oransje nivå, B4, 18 februar",
-        }),
-      },
-      "2": {
-        getElementsByTagName: (tag) => ({
-          textContent: "Another incident, no danger level",
-        }),
-      },
-      "3": {
-        getElementsByTagName: (tag) => ({
-          textContent: "Another incident, nivå and oransje not in danger level",
-        }),
-      },
-    };
-  
-    it("should return an array of incidents with danger level 'nivå' and 'oransje'", () => {
-      const result = sortByDangerLevel(testArray);
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(testArray["1"]);
-    });
-  });
+test('sortByDanger function can sort by danger', async () =>
+{
+    let xmlData = await fetchData("https://api.met.no/weatherapi/metalerts/1.1?show=all")
+    let testArr = xmlData.getElementsByTagName("title")
+    console.log(testArr)
+
+    expect(testArr.length).toBe(10);
+})
