52dbd7cb89e7243017a83d8f358ec59f2656ad69
oscarva@stud.ntnu.no <oscarva@stud.ntnu.no>
2023-02-21 14:16:45 +0100

    added  a simple counter using useReducer instead of useState for practice

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
+import Counter from "./Counter"
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
+    <Counter></Counter>
     <App />
+    
     </StrictMode>
 );
