32540a3f58d50481e31ab8ec84701b4ea26abd87
oscarva@stud.ntnu.no <oscarva@stud.ntnu.no>
2023-02-21 17:36:51 +0100

    removed Counter.js since I am not needing it anymore, removed import statement from index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import App from "./App";
-import Counter from "./Counter"
 
 const root = createRoot(document.getElementById("root"));
 root.render(
